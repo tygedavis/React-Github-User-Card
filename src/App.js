88b3366630f772d846e50b16605bcd0df41c8cd4
tygedavis <tyge.davis@gmail.com>
2019-12-03 15:27:33 -0700

    Initial card is made and renders on the screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends React.Component {
+
+  state = {
+    users: []
+  };
+
+  componentDidMount() {
+    console.log('CDM Invoked')
+
+    axios.get('https://api.github.com/users/tygedavis')
+    .then(res => {
+      console.log('GitHub Data: ', res)
+      this.setState({ users: [...this.state.users, res.data] })
+      console.log(this.state.users)
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>GitHub User Card</h1>
+
+        <div className='userCard'>
+          {this.state.users.map(user => 
+            <div key={user.id}>
+              <h2>{user.name}</h2>
+              <p>Bio: {user.bio}</p>
+              <p>Followers: {user.followers}</p>
+              <p>Following: {user.following}</p>
+            </div>
+          )}  
+        </div>
+      </div>
+    );
+  };
+};
 
 export default App;
